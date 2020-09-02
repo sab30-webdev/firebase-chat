178ce9b25f2d292b1ea491126f669eb19c189cba
sab30-webdev <sabarinath3072001@gmail.com>
2020-09-02 11:35:36 +0530

    Realtime data base of user data with Sign In with google

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
+import "rsuite/dist/styles/rsuite-default.css";
+import "./styles/main.scss";
 
 function App() {
   return (
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect, Route } from "react-router";
 
 const PrivateRoute = ({ children, ...props }) => {
-  const profile = true;
+  const profile = false;
 
   if (!profile) {
     return <Redirect to="/signin" />;
